5fcbbdc3f46955682db1c3d188c6796ebae3f99d
tennmusu <96242104+tennmusu@users.noreply.github.com>
2022-02-07 23:45:00 +0900

    Add files via upload

--- Sudoku.py
+++ Sudoku.py
@@ -5,27 +5,34 @@ import random
 import itertools
 from sqlalchemy import false
 from sympy import re
+import numpy as np
 pg.init()
 screen = pg.display.set_mode((800, 600))
-l = [1,2,3, 4,5,6,7,8,9]
 ##  問題データ
-pro=[[0 for j in range(9)] for i in range(9)]
-for yy in range(9):
-    if yy==0:
-        pro[0]
-        p = list(itertools.permutations(l))
-        pro[0]=p[random.randint(0,23)]
-        continue
-    for xx in range(9):
-        l = [1,2,3, 4,5,6,7,8,9]
-        for k in range(yy-1):
-            del l[pro[k][xx]]
-        if xx!=0:
-            for k in range(xx-1):
-                del l[pro[yy][k]]
-        pro[yy][xx]=l[random.randint(0,len(l)-1)]
+pro=np.array([
+    [1,4,7,2,5,8,3,6,9],
+    [2,5,8,3,6,9,4,7,1],
+    [3,6,9,4,7,1,5,8,2],
+
+    [4,7,1,5,8,2,6,9,3],
+    [5,8,2,6,9,3,7,1,4],
+    [6,9,3,7,1,4,8,2,5],
+    
+    [7,1,4,8,2,5,9,3,6],
+    [8,2,5,9,3,6,1,4,7],
+    [9,3,6,1,4,7,2,5,8]])
+isvar=[ True for i in range(81)]
+        
 ##  入力データ
+print(pro)
 board =[-1 for i in range(81)]
+for yy in range(9):
+    a=random.randint(0,4)
+    x = random.sample(range(0,8),a)
+    for i in x:
+        board[yy*9+i]=pro[yy][i]
+        isvar[yy*9+i]=False
+print(board)
 ## ブロックデータ
 i=0
 block=[-1 for i in range(81)]
@@ -105,21 +112,29 @@ def button_to_jump(btn, newpage):
 ## ゲームステージ
 def gamestage():
     # 3.画面を初期化する
-    global page,score,block,sel,selin,board,selnum,presel,tnum    
-    global vx, vy
+    global page,score,block,sel,selin,board,selnum,tnum,presel,isvar  
     screen.fill(pg.Color("WHITE"))
     (mx, my) = pg.mouse.get_pos()
     buttons= pg.mouse.get_pressed()
     index=1
+    pg.draw.rect(screen,(135,206,235),pg.Rect(600,390,70,50))
+    font = pg.font.Font(None, 30)
+    text = font.render("done!", True, pg.Color("BLACK"))
+    screen.blit(text, (607, 405))  
+    if (mx>=600 and mx<600+70 and my>=390 and my<390+50 and buttons[0]):
+        isdone()
     for i     in input:
         pg.draw.rect(screen, pg.Color("BLACK"), i, width=1)
         if (my>=i.y and my<i.y+30 and mx>=i.x and mx<i.x+30 and buttons[0]):
                 selin=i
-            
+                if selin==presel:
+                    tnum+=1
+                presel=i
         if selin==i:
                 pg.draw.rect(screen,(165, 197, 246), i)
-                selnum=index   
-                tnum+=1        
+                if selin!=presel:
+                    tnum==0
+                selnum=index
         else:
                  pg.draw.rect(screen,pg.Color("WHITE"), i)
         font = pg.font.Font(None, 30)
@@ -130,67 +145,58 @@ def gamestage():
     
     for q , rect in enumerate(block):
         if q==81: break
-        if (my>=rect.y and my<rect.y+30 and mx>=rect.x and mx<rect.x+30 and buttons[0]):
+        if isvar[q]==False:
+                pg.draw.rect(screen,(165, 197, 246), rect)
+                pg.draw.rect(screen, pg.Color("BLACK"), rect, width=1)
+                font = pg.font.Font(None, 30)
+                text = font.render(str(board[q]), True, pg.Color("BLACK"))   
+        if (my>=rect.y and my<rect.y+30 and mx>=rect.x and mx<rect.x+30 and buttons[0] and isvar[q]):
             sel=rect
-            
+            tnum=0
         if sel==rect:
                 pg.draw.rect(screen, pg.Color("GOLD"), rect)
-                if selnum!=0:
-                    text = font.render(str(selnum), True, pg.Color("BLACK"))
-                    if board[q]==-1:  
-                        board[q]=0
-                        if presel==0:
-                            board[q]=selnum
-                        presel=selnum 
-                    elif board[q]==0:
-                        if presel!=selnum:
-                         board[q]=selnum           
-                    else:
-                        screen.blit(text, (rect.x+9, rect.y+7))
-                        board[q]=selnum
+                if tnum!=0:
+                     board[q]=selnum
+                     text = font.render(str(selnum), True, pg.Color("BLACK"))
+                     screen.blit(text, (rect.x+9, rect.y+7))
+                elif tnum==0 and board[q]!=-1:
+                     text = font.render(str(board[q]), True, pg.Color("BLACK"))
+                     screen.blit(text, (rect.x+9, rect.y+7))
                 pg.draw.rect(screen, pg.Color("BLACK"), rect, width=1)   
         else:
                 pg.draw.rect(screen, pg.Color("BLACK"), rect, width=1)
-                if not(board[q]==-1 or board[q]==0):
+                if not(board[q]==-1):
                    text = font.render(str(board[q]), True, pg.Color("BLACK"))
                    screen.blit(text, (rect.x+9, rect.y+7))
         
 def gamereset():
- global score
- block=[]
-for zz in range(9):
-    for yy in range(3):
-        for xx in range(3):
-            if yy==0:
-                if xx==0:
-                    if zz<=2:
-                        block.append(pg.Rect(xx*30+50+88*zz, yy*30+50, 30, 30))
-                    elif zz<=5:
-                        block.append(pg.Rect(xx*30+50+88*(zz-3), yy*30+50+88, 30, 30))                       
-                    else:
-                        block.append(pg.Rect(xx*30+50+88*(zz-6), yy*30+50+88*2, 30, 30))
-                else:
-                    if zz<=2:
-                        block.append(pg.Rect(xx*30+50-(xx+1)/2+88*zz, yy*30+50, 30, 30))
-                    elif zz<=5:
-                        block.append(pg.Rect(xx*30+50-(xx+1)/2+88*(zz-3), yy*30+50+88, 30, 30))                       
-                    else:
-                        block.append(pg.Rect(xx*30+50-(xx+1)/2+88*(zz-6), yy*30+50+88*2, 30, 30))             
-            else:
-                if xx==0:
-                     if zz<=2:
-                        block.append(pg.Rect(xx*30+50+88*zz, yy*30+50-(yy+1)/2, 30, 30))
-                     elif zz<=5:
-                        block.append(pg.Rect(xx*30+50+88*(zz-3),  yy*30+50-(yy+1)/2+88, 30, 30))                       
-                     else:
-                        block.append(pg.Rect(xx*30+50+88*(zz-6),  yy*30+50-(yy+1)/2+88*2, 30, 30)) 
-                else:
-                     if zz<=2:
-                        block.append(pg.Rect(xx*30+50-(xx+1)/2+88*zz, yy*30+50-(yy+1)/2, 30, 30))
-                     elif zz<=5:
-                        block.append(pg.Rect(xx*30+50-(xx+1)/2+88*(zz-3),  yy*30+50-(yy+1)/2+88, 30, 30))                       
-                     else:
-                        block.append(pg.Rect(xx*30+50-(xx+1)/2+88*(zz-6),  yy*30+50-(yy+1)/2+88*2, 30, 30))   
+ global score,block,sel,selin,board,selnum,tnum,presel,isvar  
+ score = 0
+ sel=pg.rect
+ selin=pg.rect
+ selnum=0
+ presel=0
+ tnum=0
+ isvar=[True for i in range(81)]
+ board =[-1 for i in range(81)]
+ pro=np.array([
+    [1,4,7,2,5,8,3,6,9],
+    [2,5,8,3,6,9,4,7,1],
+    [3,6,9,4,7,1,5,8,2],
+
+    [4,7,1,5,8,2,6,9,3],
+    [5,8,2,6,9,3,7,1,4],
+    [6,9,3,7,1,4,8,2,5],
+    
+    [7,1,4,8,2,5,9,3,6],
+    [8,2,5,9,3,6,1,4,7],
+    [9,3,6,1,4,7,2,5,8]])
+ for yy in range(9):
+    a=random.randint(0,4)
+    x = random.sample(range(0,8),a)
+    for i in x:
+        board[yy*9+i]=pro[yy][i]
+        isvar[yy*9+i]=False
 def gameclear():
  gamereset()
  screen.fill(pg.Color("GOLD"))
@@ -200,6 +206,31 @@ def gameclear():
  btn1 = screen.blit(replay_img,(320, 480))
  # 5.絵を描いたり、判定したりする
  button_to_jump(btn1, 1)
+ # クリア判定
+def isdone():
+    global board,pro,page
+    isfill=True
+    score=0
+    for i in range(81):
+        if board[i]==-1:
+            isfill=False
+            font = pg.font.Font(None, 30)
+            text = font.render("Fill all the squares!", True, pg.Color("RED"))
+            screen.blit(text, (607, 305))
+            break
+    if isfill:
+        for i in range(81):
+            if board[i]!=pro[i//9][i%9]:
+                score+=1
+        font = pg.font.Font(None, 30)
+        text = font.render("You got "+str(score)+" wrong.", True, pg.Color("RED"))
+        screen.blit(text, (607, 305))
+        if score==0:
+            page=2
+
+    
+    
+
 
 # 2.この下をずっとループする
 while True:
--- pro.py
+++ pro.py
@@ -1,30 +1,35 @@
-import itertools
+import pygame as pg, sys
 import random
-l = [1,2,3, 4,5,6,7,8,9]
+import itertools
+from sqlalchemy import false
+from sympy import re
+import numpy as np
 ##  問題データ
-pro=[[1,4,7,2,5,8,3,6,9],
-    [2,5,8,3,6,9,4,6,1],
+pro=np.array([[1,4,7,2,5,8,3,6,9],
+    [2,5,8,3,6,9,4,7,1],
     [3,6,9,4,7,1,5,8,2],
     [4,7,1,5,8,2,6,9,3],
     [5,8,2,6,9,3,7,1,4],
     [6,9,3,7,1,4,8,2,5],
     [7,1,4,8,2,5,9,3,6],
     [8,2,5,9,3,6,1,4,7],
-    [9,3,6,1,4,7,2,5,8]]
+    [9,3,6,1,4,7,2,5,8]])
+isvar=[ True for i in range(81)]
 a=random.randint(0,3)
 b=random.randint(0,3)
 if a==1:
-    pro[0:3],pro[3:6]=pro[3:6],pro[0:3]
+    pro[0:2],pro[3:5]=pro[3:5],pro[0:2].copy()
 elif a==2:
-    pro[0:3],pro[6:9]=pro[6:9],pro[0:3]
+    pro[0:2],pro[6:8]=pro[6:8],pro[0:2].copy()
 elif a==3:
-    pro[3:6],pro[6:9]=pro[6:9],pro[3:6]
+    pro[3:5],pro[6:8]=pro[6:8],pro[3:5].copy()
 if b==1:
-    pro[0:9][0:3],pro[0:9][3:6]=pro[0:9][3:6],pro[0:9][0:3]
+    pro[:,0:2],pro[:,3:5]=pro[:,3:5],pro[:,0:2].copy()
 elif b==2:
-    pro[0:9][0:3],pro[0:9][6:9]=pro[0:9][6:9],pro[0:9][0:3]
+    pro[:,0:2],pro[:,6:8]=pro[:,6:8],pro[:,0:2].copy()
 elif b==3:
-    pro[0:9][3:6],pro[0:9][6:9]=pro[0:9][6:9],pro[0:9][3:6]
+    pro[:,3:5],pro[:,6:8]=pro[:,6:8],pro[:,3:5].copy()
+
 c=random.randint(10,15)
 
 for i  in range(c):
@@ -33,45 +38,44 @@ for i  in range(c):
     if i%2==0:
         if b==1:
             if a==1:
-                pro[0],pro[2]=pro[2],pro[0]
+                pro[0],pro[2]=pro[2],pro[0].copy()
             elif a==2:
-                pro[1],pro[2]=pro[2],pro[1]
+                pro[1],pro[2]=pro[2],pro[1].copy()
             elif a==3:
-                pro[0],pro[1]=pro[1],pro[0]
+                pro[0],pro[1]=pro[1],pro[0].copy()
         elif b==2:
             if a==1:
-                pro[3],pro[5]=pro[5],pro[3]
+                pro[3],pro[5]=pro[5],pro[3].copy()
             elif a==2:
-                pro[4],pro[5]=pro[5],pro[4]
+                pro[4],pro[5]=pro[5],pro[4].copy()
             elif a==3:
-                pro[3],pro[4]=pro[4],pro[3]
+                pro[3],pro[4]=pro[4],pro[3].copy()
         else:
             if a==1:
-                pro[6],pro[8]=pro[8],pro[6]
+                pro[6],pro[8]=pro[8],pro[6].copy()
             elif a==2:
-                pro[7],pro[8]=pro[8],pro[7]
+                pro[7],pro[8]=pro[8],pro[7].copy()
             elif a==3:
-                pro[6],pro[7]=pro[7],pro[6]
+                pro[6],pro[7]=pro[7],pro[6].copy()
     else:    
         if b==1:
             if a==1:
-                pro[0:9][0],pro[0:9][2]=pro[0:9][2],pro[0:9][0]
+                pro[:,0],pro[:,2]=pro[:,2],pro[:,0].copy()
             elif a==2:
-                pro[0:9][1],pro[0:9][2]=pro[0:9][2],pro[0:9][1]
+                pro[:,1],pro[:,2]=pro[:,2],pro[:,1].copy()
             elif a==3:
-                pro[0:9][0],pro[0:9][1]=pro[0:9][1],pro[0:9][0]
+                pro[:,0],pro[:,1]=pro[:,1],pro[:,0].copy()
         elif b==2:
             if a==1:
-                pro[0:9][3],pro[0:9][5]=pro[0:9][5],pro[0:9][3]
+                pro[:,3],pro[:,5]=pro[:,5],pro[:,3].copy()
             elif a==2:
-                pro[0:9][4],pro[0:9][5]=pro[0:9][5],pro[0:9][4]
+                pro[:,4],pro[:,5]=pro[:,5],pro[:,4].copy()
             elif a==3:
-                pro[0:9][3],pro[0:9][4]=pro[0:9][4],pro[0:9][3]
+                pro[:,3],pro[:,4]=pro[:,4],pro[:,3].copy()
         elif b==3:
             if a==1:
-                pro[0:9][6],pro[0:9][8]=pro[0:9][8],pro[0:9][6]
+                pro[:,6],pro[:,8]=pro[:,8],pro[:,6].copy()
             elif a==2:
-                pro[0:9][7],pro[0:9][8]=pro[0:9][8],pro[0:9][7]
+                pro[:,7],pro[:,8]=pro[:,8],pro[:,7].copy()
             elif a==3:
-                pro[0:9][6],pro[0:9][7]=pro[0:9][7],pro[0:9][6]
-print(pro)
+                pro[:,6],pro[:,7]=pro[:,7],pro[:,6].copy()
